9c17ec6342ed3c34bd073f25bfd1082bf38e85b0
harshvardhan-dot <vardhanharsh012345@gmail.com>
2026-06-30 18:05:17 +0530

    commit

--- threenumbers.c
+++ threenumbers.c
@@ -15,7 +15,7 @@ int main() {
         printf("ALL NUMBERS ARE EQUAL");
      } 
     else if(a>b&&a>c){
-        printf("A IS largest");
+        printf("a IS largest");
     }
     else if(b>a&&b>c){
         printf("A IS largest");
