543420cc279ed6ef1652de424034064b3177f965
harshvardhan-dot <vardhanharsh012345@gmail.com>
2026-06-30 16:34:59 +0530

    added fibonacci series

---
+++ iterativefibo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibonacciIterative(int n) {
+    int a = 0, b = 1, c;
+    if (n == 0) return a;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n = 10;
+    printf("Iterative Fibonacci at position %d = %d\n", n, fibonacciIterative(n));
+    return 0;
+}
